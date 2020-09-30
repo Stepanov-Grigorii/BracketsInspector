c69dd25edc537427414b96f6ca01bf6df703b669
Stepanov-Grigorii <stegrand@yandex.ru>
2020-09-30 20:50:40 +0300

    Better Version

---
+++ src/com/company/AltMain.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.Stack;
+
+public class AltMain {
+    public static void main(String[] args) throws IOException {
+        String strFileContent = Files.readString(Path.of("../BracketsInspector/src/com/company/resources/text.txt"));
+        System.out.println("strFileContent = " + strFileContent);
+        boolean check = check(strFileContent);
+        System.out.println("check = " + check);
+    }
+    static boolean check(String content){
+        Map<Character, Character> mapOfBrackets = Map.of(
+                ')', '(',
+                '}', '{',
+                ']', '['
+        );
+        Stack<Character> brackets = new Stack<>();
+        for (int i = 0; i < content.length(); i++) {
+            Character current = content.charAt(i);
+            if(mapOfBrackets.containsValue(current)){
+                brackets.push(current);
+                continue;
+            }
+            if(mapOfBrackets.containsKey(current) && (brackets.isEmpty() || !brackets.pop().equals(mapOfBrackets.get(current)))){
+                return false;
+            }
+        }
+        return brackets.isEmpty();
+    }
+}
